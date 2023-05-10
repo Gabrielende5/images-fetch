2497d81934889455058eacc6420f49a23fb22342
Gabrielende5 <gabrielende53@gmail.com>
2023-05-10 20:56:30 -0300

    tarefa

--- post.js
+++ post.js
@@ -1,24 +1,39 @@
 const form = document.getElementById("my-form");
 form.addEventListener("submit",handleSubmit);
-
-function handleSubmit(event){
-    event.preventDefault();
-    const data = new FormData();
-    data.append("post[title]",event.target.title.value);
-    data.append("post[image]",event.target.image.files[0]);
-    console.log(data);
-    submitToAPI(data);
-
+var colorCard = 'Card-de-envio';
+var textCard =''
+function handleSubmit(event) {
+  event.preventDefault();
+  const data = new FormData();
+  data.append("post[title]",event.target.title.value);
+  data.append("post[image]",event.target.image.files[0]);
+  submitToAPI(data);
 }
+const miniCard = document.getElementById("Cards");
 async function submitToAPI(data){
-    var response = await fetch("http://localhost:3000/posts",{
-        method: "POST",
-        body: data,
+  try {
+    var response = await fetch("http:/localhost:3000/posts",{
+      method: "POST",
+      body: data,
     })
-
     var json = await response.json()
-    console.log(json)
     const latestPost = document.getElementById("latest-post");
-    latestPost.innerHTML = `<img src="${json.image_url}" alt="ultima Imagem"/>`
-
+    latestPost.innerHTML = `<img class="LatestImagem" src="${json.image_url}" alt="ultima Imagem" />`;
+    var colorCard = 'Card-de-envio';
+    var textCard= 'Enviado'
+    endAnimation()
+  } catch (error) {
+    var colorCard = 'Card-de-erro';
+    var textCard = "Erro";
+    endAnimation()
+  }
+  miniCard.innerHTML = `<div id="card" class='${colorCard}'> ${textCard}</div>`  
+  
+  endAnimation()
+}
+function endAnimation(){
+  setTimeout(()=>{
+    const card = document.getElementById("card");
+    card.style.opacity = 0;
+  },2000)
 }
--- styles.js
+++ styles.js
@@ -3,8 +3,36 @@ body.classList.add("body-dark");
 function darkTheme(){
     body.classList.add("body-dark");
     body.classList.remove("body-light");
+    body.classList.remove("body-cyan");
 }
 function lightTheme(){
     body.classList.add("body-light");
     body.classList.remove("body-dark");
+    body.classList.remove("body-cyan");
 }
+function cyanTheme(){
+    body.classList.add("body-cyan");
+    body.classList.remove("body-light");
+    body.classList.remove("body-dark");
+}
+const fileInput = document.getElementById("image");
+const imageLabel = document.getElementById("imageLabel");
+
+fileInput.addEventListener("change", function() {
+  if (fileInput.files.length > 0 && fileInput.files[0].type.startsWith("image/")) {
+    // A file has been selected and it's an image
+    const reader = new FileReader();
+    reader.onload = function(event) {
+      imageLabel.innerHTML = ''
+      // Set the background-image of the imageLabel to the selected image
+      imageLabel.style.backgroundImage = `url(${event.target.result})`;
+    }
+
+    reader.readAsDataURL(fileInput.files[0]);
+  } else {
+    // No file selected or the selected file is not an image
+    imageLabel.style.backgroundImage = "";
+    
+    imageLabel.innerHTML = 'Envie aqui sua imagem'
+  }
+});
